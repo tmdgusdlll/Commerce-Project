b7a1915f63416b4f6fe10410cdd0f8c2b3528f55
tmdgsudl <leesh27955@gmail.com>
2026-01-19 19:28:26 +0900

    [step2] Main에서 담당하던 입출력 반복문 CommerceSystem클래스에서 start() 함수로 관리

--- src/project/CommerceSystem.java
+++ src/project/CommerceSystem.java
@@ -2,6 +2,7 @@ package project;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.Scanner;
 
 public class CommerceSystem {
 
@@ -10,5 +11,30 @@ public class CommerceSystem {
 
     public CommerceSystem(List<Product> products) {
         this.products = products;
+    }
+
+    public void start() {
+        System.out.println("[ 실시간 커머스 플랫폼 - 전자제품 ]");
+        for (int i = 0; i < products.size(); i++) {
+            Product p = products.get(i);
+            System.out.println((i + 1) + ". " + p.getName() + "   | " + p.getPrice() + "원 | " + p.getExplain());
+        }
+        System.out.println("0. 종료           | 프로그램 종료" );
+
+        while (true) {
+            Scanner sc = new Scanner(System.in);
+            int answer;
+
+            try {
+                System.out.print("상품을 선택하세요: ");
+                answer = Integer.parseInt(sc.nextLine());
+                if (answer == 0) {
+                    System.out.println("커머스 플랫폼을 종료합니다.");
+                    break;
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("숫자만 입력해주세요.");
+            }
+        }
     }
 }
