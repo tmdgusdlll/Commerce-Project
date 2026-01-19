3f53665329443409fbeae468a12220e9dec1667e
tmdgsudl <leesh27955@gmail.com>
2026-01-19 19:24:03 +0900

    [step2] CommmerceSystem 클래스 생성 후 필드, 생성자 할당

--- src/project/CommerceSystem.java
+++ src/project/CommerceSystem.java
@@ -1,4 +1,14 @@
 package project;
 
+import java.util.Comparator;
+import java.util.List;
+
 public class CommerceSystem {
+
+    // 커머스 플랫폼의 상품을 관리하고 사용자 입력을 처리하는 클래스
+    private List<Product> products;
+
+    public CommerceSystem(List<Product> products) {
+        this.products = products;
+    }
 }
