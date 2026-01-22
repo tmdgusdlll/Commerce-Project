a719f0bb2eae491f36e8a2258bc56f1d5cf6e3c3
tmdgsudl <leesh27955@gmail.com>
2026-01-22 12:38:17 +0900

    [step4] 캡슐화 이미 진행 중! (+메인 출력문 메서드화 해봄)

--- src/project/Product.java
+++ src/project/Product.java
@@ -17,33 +17,33 @@ public class Product {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+//    public void setName(String name) {
+//        this.name = name;
+//    }
 
     public int getPrice() {
         return price;
     }
 
-    public void setPrice(int price) {
-        this.price = price;
-    }
+//    public void setPrice(int price) {
+//        this.price = price;
+//    }
 
     public String getDescription() {
         return description;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
+//    public void setDescription(String description) {
+//        this.description = description;
+//    }
 
-    public int getStock() {
-        return stock;
-    }
+//    public int getStock() {
+//        return stock;
+//    }
 
-    public void setStock(int stock) {
-        this.stock = stock;
-    }
+//    public void setStock(int stock) {
+//        this.stock = stock;
+//    }
 
     @Override
     public String toString() {
